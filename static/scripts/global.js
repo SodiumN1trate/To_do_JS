923a062a80881ef957136305d8521a6bca124459
SodiumN1trate <renarsgausins21@gmail.com>
2021-05-11 11:44:38 +0300

    Issue fix

--- static/scripts/global.js
+++ static/scripts/global.js
@@ -6,13 +6,13 @@ window.addEventListener('load', () => {
     let taskDate = document.getElementById("timePlace");
 
     let taskList = JSON.parse(localStorage.getItem("taskList")) || [];  // All task list
-    console.log(taskList);
 
     render();
 
     // Add task to list
     ADD_BUTTON.addEventListener("click", () => {
         if (task.value === "" || taskDate.value === "") {
+            document.getElementById("errorMessage").innerHTML = "Aizpildiet visus vajadzīgos laukus!";
             document.getElementById("errorMessage").style.display = "initial";
         }
         else if (taskDate.value === "04:20" || taskDate.value === "4:20"){
@@ -22,21 +22,17 @@ window.addEventListener('load', () => {
             document.body.style.overflow = "hidden";
         }
         else{
-            addTask = {task: task.value, taskDate: taskDate.value, status: false};
-            taskList.push(addTask);
-            task.value = "";
-            taskDate.value = "";
-            render();
+            addTasks();
         }
     })
 
     // Done and Delete buttons functionality
-    document.querySelector("#tasks").addEventListener("click", function (e) {
+    document.getElementById("tasks").addEventListener("click", function (e) {
         if(e.target.className === "doneButton"){ // When clicked "Done" button
             e.target.parentNode.style.backgroundColor = "#47DF21"; // Change task background-color because it's done
             e.target.style.display = "none"; // Hide "Done" button
             taskList.forEach(element => { // Find the task which done, and then change "done" status to true. This we need when we add new task to see if other tasks already done
-                if (element.task === e.target.parentNode.childNodes[1].innerHTML)
+                if (element.task === e.target.parentNode.childNodes[1].innerHTML || element.taskDate === e.target.parentNode.childNodes[3].innerHTML)
                 {
                     element.status = true;
                     render();
@@ -47,14 +43,33 @@ window.addEventListener('load', () => {
         {
             e.target.parentNode.parentElement.removeChild(e.target.parentNode); // Remove task from list
             taskList.forEach(element => {
-                if (element.task === e.target.parentNode.childNodes[1].innerHTML) {
+                if (element.task === e.target.parentNode.childNodes[1].innerHTML || element.taskDate === e.target.parentNode.childNodes[3].innerHTML) {
                     let index = taskList.indexOf(element); // Delete task from task list
                     taskList.splice(index, 1);
                     render();
                 }
             })
         }
+        return;
     })
+
+
+    function addTasks() {
+        for (let index = 0; index < taskList.length; index++) {
+            if (taskList[index].task == task.value){
+                document.getElementById("errorMessage").innerHTML = "Tāds uzdevums jau ir pievienots!";
+                document.getElementById("errorMessage").style.display = "initial";
+                return 0;
+            }
+        }
+        addTask = {task: task.value, taskDate: taskDate.value, status: false};
+        taskList.push(addTask);
+        task.value = "";
+        taskDate.value = "";
+        document.getElementById("errorMessage").innerHTML = "";
+        document.getElementById("errorMessage").style.display = "none";
+        render();
+    }
 
     // Function which renders new and done tasks.
     function render() {
@@ -65,7 +80,7 @@ window.addEventListener('load', () => {
             if (e.status === false)
             {
                 taskCode = `
-                <div class="taskForm">
+                <div id="taskForm">
                     <span class="task">${e.task}</span>  -  <span class="time">${e.taskDate}</span>
                     <input type="button" class="deleteButton" value="Dzēst">
                     <input type="button" class="doneButton" value="Pabeigt">
@@ -73,7 +88,7 @@ window.addEventListener('load', () => {
             }
             else{
                 taskCode = `
-                <div class="taskForm" style="background-color: #47DF21;">
+                <div id="taskForm" style="background-color: #47DF21;">
                     <span class="task">${e.task}</span>  -  <span class="time">${e.taskDate}</span>
                     <input type="button" class="deleteButton" value="Dzēst">
                 </div>`;
